c5c9572c60554f99f378ac4a6bcac73125011065
riniaputhoor <rini.a.puthoor@gmail.com>
2026-02-28 18:46:20 +0530

    Add files via upload

---
+++ Test.js
@@ -0,0 +1,12 @@
+var firstName="Rini";
+var companyName="DBS";
+var mobileNumber=9908765545
+var isAutomation=true;
+var hasPlaywright;
+
+
+console.log(typeof firstName);
+console.log(typeof companyName);
+console.log(typeof mobileNumber);
+console.log(typeof isAutomation);
+console.log(typeof hasPlaywright);
